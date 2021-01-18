4e2690d01105224e553ca728e7b3700d19c17517
Wen Jen <wjen1214@gmail.com>
2021-01-18 14:46:10 -0800

    setup routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from '/Navbar';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Error from './pages/Error';
+import SingleDrink from './pages/Error';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/drink/:id" component={SingleDrink}></Route>
+        <Route path="*" component={Error}></Route>
+      </Switch>
     </Router>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-
-  return (
-
-  )
+  return <div> This is a navbar</div>;
 }
 
 export default Navbar;
